dc15b18bd80e86055049ebca21aadc26ab71ccca
shivamvora <shivampatel27102001@gmail.com>
2022-04-24 16:45:19 +0530

    update code

--- ecommerce/src/components/Navbar.jsx
+++ ecommerce/src/components/Navbar.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+padding:10px 20px ;
+display:flex ;
+align-items:center ;
+justify-content:space-between ;
+`;
+
+const Left = styled.div`
+flex:1 ;
+display:flex ;
+align-items:center ;
+`;
+const Language = styled.span`
+  font-size:14px ;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border:0.5px solid lightgray;
+  display:flex ;
+  align-items:center ;
+  margin-left:25px ;
+  padding:5px ;
+`;
+
+const Input = styled.input`
+border:none ;
+`;
+
+const Center = styled.div`
+flex:1 ;
+text-align:center ;
+`;
+const Logo = styled.h1`
+  font-weight:bold ;
+`;
+const Right = styled.div`
+flex:1 ;
+
+
+`;
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input/>
+            <SearchIcon/>
+          </SearchContainer>
+        </Left>
+        <Center><Logo>Shivam</Logo></Center>
+        <Right>right</Right>
+      </Wrapper>
+      </Container>
   )
 }
 
